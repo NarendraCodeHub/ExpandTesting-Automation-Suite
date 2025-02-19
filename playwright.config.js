74c9e2542dafead0e3888b23e3e3f5879a03630e
Narendra122812 <narendrakumar122812@gmail.com>
2025-02-19 18:28:30 +0530

    Modify playwright.config.js file

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://practice.expandtesting.com/',
-    headless: false,
+    headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
